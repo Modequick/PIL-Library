ba2e22dbe4dc82770fceb71185fd605d31caf290
ModeQuick <kevinadrianh@yahoo.com>
2022-03-14 18:23:09 +0700

    "My First Commit "

---
+++ PILEXAMPLE.py
@@ -0,0 +1,19 @@
+import PIL
+from PIL import Image
+import os
+os.chdir(r"C:\Users\kevin\OneDrive\Pictures\Matthew")
+
+isi = os.listdir(r"C:\Users\kevin\OneDrive\Pictures\Matthew")
+
+
+for i in isi : 
+    if i.startswith("foto"):
+        foto = Image.open(i)
+        foto.rotate(-90).save(r"C:\Users\kevin\OneDrive\Pictures\Matthew\hasil\ " + i , "JPEG")
+        print(i)
+    
+
+
+#pubesar= foto.rotate(0).resize((1920,1080))
+#os.chdir(r"C:\Users\kevin\OneDrive\Pictures\Matthew\trial123")
+#pubesar.save("C:\Users\kevin\OneDrive\Pictures\Matthew\trial123", )
